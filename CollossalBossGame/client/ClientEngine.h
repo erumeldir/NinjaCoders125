a3f6b7bff9e4acbb1da0ddc0b778593fca2b05c8
Nathan Alexander Heisey <nathanheisey@gmail.com>
2013-04-08 13:21:26 -0700

    Basic skeleton of the engine created

--- CollossalBossGame/client/ClientEngine.h
+++ CollossalBossGame/client/ClientEngine.h
@@ -4,9 +4,21 @@
  */
 #ifndef CLIENT_ENGINE_H
 #define CLIENT_ENGINE_H
+
 class ClientEngine {
 public:
+	static void init() { ce = new ClientEngine(); }
+	static ClientEngine *get() { return ce; }
+	static void clean() { delete ce; }
+
+	void run();
+
 private:
+	//Constructors/destructors are private
+	ClientEngine();
+	virtual ~ClientEngine();
+
+	static ClientEngine *ce;	//instance
 };
 
 #endif
--- CollossalBossGame/client/main.cpp
+++ CollossalBossGame/client/main.cpp
@@ -2,4 +2,14 @@
  * main.cpp
  */
 
-#include "
+#include "ClientEngine.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Hello world!\n");
+
+	ClientEngine::init();
+	ClientEngine::get()->run();
+	ClientEngine::clean();
+	return 0;
+}
