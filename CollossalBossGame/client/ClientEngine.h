3529ddab1a3b24716653abea6fd166b745f390d9
Nathan Alexander Heisey <nathanheisey@gmail.com>
2013-04-08 13:07:22 -0700

    Initial project setup created

---
+++ CollossalBossGame/client/ClientEngine.h
@@ -0,0 +1,12 @@
+/*
+ * ClientEngine.h
+ * Defines the main game engine for the client and the server.
+ */
+#ifndef CLIENT_ENGINE_H
+#define CLIENT_ENGINE_H
+class ClientEngine {
+public:
+private:
+};
+
+#endif
