65bdecc44780de9ba225237d4a237c8e10bbddb6
Nathan Alexander Heisey <nathanheisey@gmail.com>
2013-04-08 13:21:03 -0700

    Basic skeleton of the engine created

---
+++ CollossalBossGame/client/ClientEngine.cpp
@@ -0,0 +1,27 @@
+/*
+ * ClientEngine.cpp
+ */
+
+#include "ClientEngine.h"
+
+//Static members
+ClientEngine *ClientEngine::ce;
+
+
+/*
+ * Engine constructor.  Initializes the other engines.
+ */
+ClientEngine::ClientEngine() {
+}
+
+/*
+ * Engine destructor.  Cleans the other engines.
+ */
+ClientEngine::~ClientEngine() {
+}
+
+/*
+ * The main game loop of the engine.  Initializes and runs the game.
+ */
+void ClientEngine::run() {
+}
