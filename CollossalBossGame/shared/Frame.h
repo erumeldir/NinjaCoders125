d13930ce70fd00fc0a63ceab3e8dc452ff4d7f81
Nathan Heisey <nathanheisey@gmail.com>
2013-04-10 21:19:54 -0700

    Made points passed by reference

--- CollossalBossGame/shared/Frame.h
+++ CollossalBossGame/shared/Frame.h
@@ -19,8 +19,8 @@ public:
 	void translate(const Vec3f &dv);
 	void rotate(const Vec3f &dr);
 
-	void setPos(Point_t pt) { pos = pt; }
-	void setRot(Rot_t rt) { rot = rt; }
+	void setPos(const Point_t &pt) { pos = pt; }
+	void setRot(const Rot_t &rt) { rot = rt; }
 	Point_t getPos() { return pos; }
 	Rot_t getRot() { return rot; }
 private:
