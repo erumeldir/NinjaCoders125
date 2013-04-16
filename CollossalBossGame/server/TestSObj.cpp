1fab31d429be94be22615e380eb5476acf1368eb
Nathan Alexander Heisey <nathanheisey@gmail.com>
2013-04-16 10:42:59 -0700

    Added some test movement to test server objects

--- CollossalBossGame/server/TestSObj.cpp
+++ CollossalBossGame/server/TestSObj.cpp
@@ -1,11 +1,12 @@
 #include "TestSObj.h"
+#include <math.h>
 
-
-TestSObj::TestSObj(uint id, Model modelNum, Point_t pos) : ServerObject(id) {
+TestSObj::TestSObj(uint id, Model modelNum, Point_t pos, int dir) : ServerObject(id) {
 	DC::get()->print("Created new TestSObj %d\n", id);
 	pm = new PhysicsModel(pos, Rot_t(), 500);
-
+	this->dir = dir;
 	this->modelNum = modelNum;
+	t = 0;
 }
 
 
@@ -13,6 +14,32 @@ TestSObj::~TestSObj(void) {
 }
 
 bool TestSObj::update() {
+#define MOVE_AMT 1
+#define DIV 25
+	/*
+	 * North = +Z (away from player start)
+	 * East  = +X (right of player start)
+	 * South = -Z (towards player start)
+	 * West  = -X (left of player start)
+	 */
+	switch(dir) {
+	case TEST_NORTH:
+		pm->applyForce(Vec3f(0, 0, MOVE_AMT * sin((double)t / DIV)));
+		break;
+	case TEST_EAST:
+		pm->applyForce(Vec3f(MOVE_AMT * sin((double)t / DIV), 0, 0));
+		break;
+	case TEST_SOUTH:
+		pm->applyForce(Vec3f(0, 0, -MOVE_AMT * sin((double)t / DIV)));
+		break;
+	case TEST_WEST:
+		pm->applyForce(Vec3f(-MOVE_AMT * sin((double)t / DIV), 0, 0));
+		break;
+	default:
+		break;
+	}
+	++t;
+
 	return false;
 }
 
--- CollossalBossGame/server/TestSObj.h
+++ CollossalBossGame/server/TestSObj.h
@@ -3,9 +3,15 @@
 #include "Action.h"
 #include "NetworkData.h"
 
+#define TEST_NORTH 0
+#define TEST_SOUTH 1
+#define TEST_EAST  2
+#define TEST_WEST  3
+#define TEST_STILL 4
+
 class TestSObj : public ServerObject {
 public:
-	TestSObj(uint id, Model modelNum, Point_t pos);
+	TestSObj(uint id, Model modelNum, Point_t pos, int dir = TEST_STILL);
 	virtual ~TestSObj(void);
 
 	virtual bool update();
@@ -18,5 +24,7 @@ public:
 private:
 	PhysicsModel *pm;
 	Model modelNum;
+	int dir;
+	int t;
 };
 
